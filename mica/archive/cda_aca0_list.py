30ff055c99538b2da8d51280d50bcc82225a8dc3
Jean Connelly <jconnelly@cfa.harvard.edu>
2012-12-03 11:21:15 -0500

    Generalize creation of table from text table and add more derived columns to the table.  Major revision.

--- mica/archive/cda_aca0_list.py
+++ mica/archive/cda_aca0_list.py
@@ -1,4 +1,5 @@
 import os
+import re
 import tables
 import asciitable
 import numpy as np
@@ -6,16 +7,19 @@ import urllib
 import mx.DateTime
 from itertools import izip
 from Chandra.Time import DateTime
+import Ska.Numpy
 import logging
 import argparse
 
 config = dict(data_root='/data/aca/archive/aca0',
-              cda_fetch_url='https://icxc.harvard.edu/dbtm/CDA/cgi/aspect_fetch.cgi',
+              cda_fetch_url=
+              'https://icxc.harvard.edu/dbtm/CDA/cgi/aspect_fetch.cgi',
               cda_table='cda_aca0.h5')
 
 logger = logging.getLogger('CDA ACA0 file list update')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
 
 def get_options():
     parser = argparse.ArgumentParser(
@@ -29,6 +33,25 @@ def get_options():
     opt = parser.parse_args()
     return opt
 
+
+def make_data_table(lines):
+    files = asciitable.read(lines,
+                            names=['filename', 'status', 'ingest_time'])
+    ingest_dates = [DateTime(mx.DateTime.strptime(
+                t, '%b%t%d%t%Y%t%I:%M%p')).date
+                    for t in files['ingest_time']]
+    file_re = re.compile(r'acaf(\d{9,})N(\d{3})_(\d)_img0.fits(\.gz)?')
+    filetimes = [int(file_re.search(f).group(1)) for f in files['filename']]
+    versions = [int(file_re.search(f).group(2)) for f in files['filename']]
+    now_dates = np.repeat(DateTime().date, len(ingest_dates))
+    files = Ska.Numpy.add_column(files, 'ingest_date', ingest_dates)
+    files = Ska.Numpy.add_column(files, 'aca_ingest', now_dates)
+    files = Ska.Numpy.add_column(files, 'filetime', filetimes)
+    files = Ska.Numpy.add_column(files, 'version', versions)
+    files.sort(order=['aca_ingest', 'ingest_date', 'filename'])
+    return files
+
+
 def make_table_from_scratch(table_file, cda_fetch_url, start='1999:001'):
     logger.info("Fetching new data from %s" % start)
     startmx = DateTime(start).mxDateTime
@@ -36,8 +59,7 @@ def make_table_from_scratch(table_file, cda_fetch_url, start='1999:001'):
              % startmx.strftime("%m-%d-%Y"))
     url = cda_fetch_url + query
     new_lines = urllib.urlopen(url).readlines()
-    files = asciitable.read(new_lines,
-                            names=['filename', 'status', 'ingest_time'])
+    files = make_data_table(new_lines)
     logger.info("Creating new table at %s" % table_file)
     h5f = tables.openFile(table_file, 'a',
                           filters=tables.Filters(complevel=5, complib='zlib'))
@@ -55,46 +77,46 @@ def update_cda_table(data_root=config['data_root'],
     table_file = os.path.join(data_root, cda_table)
     if not os.path.exists(table_file):
         make_table_from_scratch(table_file, cda_fetch_url)
-    
+
     h5f = tables.openFile(table_file, 'a')
     tbl = h5f.getNode('/', 'data')
     cda_files = tbl[:]
-    lasttime = cda_files[-1]['ingest_time']
+    lastdate = cda_files[-1]['ingest_date']
+    lastmx = DateTime(lastdate).mxDateTime
 
-    mxlast = mx.DateTime.strptime(lasttime, "%b%t%d%t%Y%t%I:%M%p")
-    logger.info("Fetching new data from %s" 
-                % mxlast.strftime("%m-%d-%Y"))
-    query = ("?tstart=%s&pattern=aca%%25&submit=Search"
-                 % mxlast.strftime("%m-%d-%Y"))
+    logger.info("Fetching new data from %s" % lastdate)
+    query = ("?tstart=%s&pattern=acaimgc%%25&submit=Search"
+                 % lastmx.strftime("%m-%d-%Y"))
     url = cda_fetch_url + query
     new_lines = urllib.urlopen(url).readlines()
-    arc_files = asciitable.read(new_lines,
-                                names = ['filename', 'status', 'ingest_time'])
-    match_first = np.flatnonzero(cda_files['filename'] == arc_files[0]['filename'])
-    if len(match_first) == 0:
+    files = make_data_table(new_lines)
+    match = np.flatnonzero(cda_files['filename'] == files[0]['filename'])
+    if len(match) == 0:
         raise ValueError("no overlap")
-    if len(match_first) > 1:
-        raise ValueError("duplicate files")
-    match_first_idx = match_first[0]
+    match_last_idx = match[-1]
 
     i_diff = 0
-    for have_entry, new_entry in izip(cda_files[match_first_idx:], arc_files):
+    for have_entry, new_entry in izip(cda_files[match_last_idx:], files):
         if have_entry['filename'] != new_entry['filename']:
             break
         i_diff += 1
 
-    if i_diff < len(arc_files):
-        logger.info("Updating %s with %d new rows" 
-                    % (table_file, len(arc_files[i_diff:])))
-        for file in arc_files[i_diff:]:
+    if i_diff < len(files):
+        logger.info("Updating %s with %d new rows"
+                    % (table_file, len(files[i_diff:])))
+        for file in files[i_diff:]:
             logger.debug(file)
             row = tbl.row
             row['filename'] = file['filename']
             row['status'] = file['status']
             row['ingest_time'] = file['ingest_time']
+            row['ingest_date'] = file['ingest_date']
+            row['aca_ingest'] = file['aca_ingest']
+            row['filetime'] = file['filetime']
             row.append()
         tbl.flush()
     h5f.close()
+
 
 def main():
     opt = get_options()
