194fcb6ef7b6132e40c956fce6222a64e4ddc324
Jean Connelly <jconnelly@cfa.harvard.edu>
2013-05-06 12:22:37 -0400

    Setup asp_l1_proc to build its table as needed

--- mica/archive/asp_l1_proc.py
+++ mica/archive/asp_l1_proc.py
@@ -11,11 +11,17 @@ import gzip
 
 mica_archive = os.environ.get('MICA_ARCHIVE') or '/data/aca/archive'
 
-def update(obsids):
+DEFAULT_CONFIG = dict(data_root=os.path.join(mica_archive, 'asp1'),
+                      sql_def='processing_asp_l1_def.sql')
+
+
+def update(obsids, config=None):
     from mica.archive import asp_l1
     """
     For a list of obsids, update the mica table of aspect 1 processing
     """
+    if config is None:
+        config = DEFAULT_CONFIG
     logger = logging.getLogger('asp 1 proc table')
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
@@ -23,10 +29,21 @@ def update(obsids):
     apstat_db = Ska.DBI.DBI(dbi='sybase',
                             server='sqlsao',
                             database='axafapstat')
-    proc_db = Ska.DBI.DBI(dbi='sqlite',
-                          server=os.path.join(mica_archive, 'asp1', 'processing_asp_l1.db3'))
+    proc_db_file = os.path.join(mica_archive, 'asp1', 'processing_asp_l1.db3')
+    if not os.path.exists(proc_db_file) or os.stat(proc_db_file).st_size == 0:
+        if not os.path.exists(config['data_root']):
+            os.makedirs(config['data_root'])
+        logger.info("creating aspect_1_proc db from {}".format(
+            config['sql_def']))
+        db_sql = os.path.join(os.environ['SKA_DATA'], 'mica', config['sql_def'])
+        db_init_cmds = file(db_sql).read()
+        proc_db = Ska.DBI.DBI(dbi='sqlite', server=proc_db_file)
+        proc_db.execute(db_init_cmds)
+    else:
+        proc_db = Ska.DBI.DBI(dbi='sqlite', server=proc_db_file)
     archdb = Ska.DBI.DBI(dbi='sqlite',
-                         server=os.path.join(mica_archive, 'asp1', 'archfiles.db3'))
+                         server=os.path.join(config['data_root'],
+                                             'archfiles.db3'))
     for obs in obsids:
         logger.info("Adding asp1 processing for obs {}".format(obs))
         asols = asp_l1.get_files(obsid=obs, content='ASPSOL', revision='all')
@@ -37,7 +54,8 @@ def update(obsids):
             aspect_log = gzip.open(logfile).read()
             # read the obspar version with a regex from the log
             obspar_version = int(
-                re.search("axaff\d{5}_\d{3}N(\d{3})_obs0a\.par", aspect_log).group(1))
+                re.search("axaff\d{5}_\d{3}N(\d{3})_obs0a\.par",
+                          aspect_log).group(1))
             hdus = fits.open(sol)
             obi = hdus[1].header['OBI_NUM']
             revision = hdus[1].header['REVISION']
