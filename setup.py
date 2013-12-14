f24463e1205b5c515a5e507c21562ab11db929bd
Tom Aldcroft <taldcroft@gmail.com>
2013-12-14 09:04:32 -0500

    Change setup.py to use distutils not setuptools; add aca_dark package

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+
 from mica.version import version
+
+license = """\
+New BSD/3-clause BSD License
+Copyright (c) 2012 Smithsonian Astrophysical Observatory
+All rights reserved."""
+
 setup(name='mica',
+      description='Mica aspects archive',
       version=str(version),
       author='Jean Connelly',
       author_email='jconnelly@cfa.harvard.edu',
-      license="""New BSD/3-clause BSD License
-Copyright (c) 2012 Smithsonian Astrophysical Observatory
-All rights reserved.""",
+      license=license,
       zip_safe=False,
-      packages=['mica', 'mica.archive', 'mica.vv',
-                'mica.starcheck', 'mica.catalog',
-                'mica.report'],
+      packages=['mica', 'mica.archive', 'mica.aca_dark', 'mica.vv',
+                'mica.starcheck', 'mica.catalog', 'mica.report'],
       )
