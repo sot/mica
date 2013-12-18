98325daae78dc85b48d68505557cbcd7c84c04b7
Tom Aldcroft <taldcroft@gmail.com>
2013-12-17 21:34:26 -0500

    Update ACA hdr3 docs to use short MSIDs

--- utilities/make_hdr3_table.py
+++ utilities/make_hdr3_table.py
@@ -1,17 +1,22 @@
+import re
+import sys
+sys.path.insert(0, '..')
+
 from mica.archive import aca_hdr3
 from astropy.table import Table
-
-mem_loc = sorted(aca_hdr3.HDR3_DEF)
-desc = ['{}'.format(aca_hdr3.HDR3_DEF[m_id]['desc'])
-        for m_id in mem_loc]
-longdesc = ['<pre>{}</pre>'.format(aca_hdr3.HDR3_DEF[m_id]['longdesc'].strip())
-            for m_id in mem_loc]
-
-t = Table([mem_loc, desc, longdesc], names=['location', 'desc', 'longdesc'])
-
 from astropy.table.pprint import _pformat_table
 
-ftable, nlines =  _pformat_table(t, max_lines=-1, max_width=-1, html=True)
-f = open('hdr3_only_msids.html')
+hdr3 = aca_hdr3.HDR3_DEF
+m_ids = sorted(hdr3)
+msids = ['<pre style="font-family:serif">{}</pre>'.format(hdr3[m_id]['msid']) for m_id in m_ids]
+descs = ['<pre style="font-family:serif">{}</pre>'.format(hdr3[m_id]['longdesc'].strip())
+         for m_id in m_ids]
+
+t = Table([msids, descs], names=['MSID', 'Description'])
+ftable, nlines = _pformat_table(t, max_lines=-1, max_width=-1, html=True)
+
+ftable[0] = re.sub(r'<table>', '<table border="1">', ftable[0])
+
+f = open('hdr3_only_msids.html', 'w')
 f.writelines(ftable)
 f.close()
