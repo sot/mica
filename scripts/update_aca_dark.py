400a36a5ebeb6eec1945b1604538276be954da36
Tom Aldcroft <taldcroft@gmail.com>
2013-12-23 11:46:35 -0500

    Move mica/aca_dark to mica/archive/aca_dark

--- scripts/update_aca_dark.py
+++ scripts/update_aca_dark.py
@@ -3,5 +3,5 @@
 Update the Mica ACA dark calibrations database.  This is normally run as a cron job.
 """
 if __name__ == '__main__':
-    from mica.aca_dark import update_aca_dark
+    from mica.archive.aca_dark import update_aca_dark
     update_aca_dark.main()
