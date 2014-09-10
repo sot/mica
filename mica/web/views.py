8b0d08247bd1db3acf3b849613b8c933c0655c1a
Tom Aldcroft <taldcroft@gmail.com>
2014-09-10 15:05:38 -0400

    Minor logic cleanup for obsid handling

--- mica/web/views.py
+++ mica/web/views.py
@@ -10,18 +10,22 @@ class IndexView(BaseView, TemplateView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(IndexView, self).get_context_data(**kwargs)
-        obsid_or_date = self.request.GET.get('obsid_or_date')
-        try:
-            obsid = int(obsid_or_date)
-        except:
-            try:
-                obsids = events.obsids.filter(start=obsid_or_date)
-                obsid = obsids[0].obsid
-            except:
-                obsid = None
 
-        context['obsid'] = obsid
+        obsid = self.request.GET.get('obsid_or_date', None)
+        print(repr(obsid))
         if obsid is not None:
+            try:
+                obsid = int(obsid)
+            except:
+                try:
+                    obsids = events.obsids.filter(start=obsid)
+                    obsid = obsids[0].obsid
+                except:
+                    obsid = None
+
+        context['obsid'] = obsid or ''
+
+        if obsid:
             obsid = format(obsid, '05d')
             url = ('https://icxc.harvard.edu/aspect/mica_reports/{}/{}/index.html'
                    .format(obsid[:2], obsid))
