819c8ea30c4414a62a7b33a923bb898ef8f744b7
Jean Connelly <jconnelly@cfa.harvard.edu>
2015-10-01 18:33:25 -0400

    Release bugfix version 0.5.1

--- mica/version.py
+++ mica/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '0.5.1dev'
+version = '0.5.1'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
