4768842be70169b778690f1c1b9431dc4e9d3c94
Jean Connelly <jconnelly@cfa.harvard.edu>
2016-08-26 12:01:33 -0400

    Use ternary to set mp_dir

--- mica/web/pcad_table.py
+++ mica/web/pcad_table.py
@@ -86,9 +86,7 @@ def get_acq_table(obsid):
     # This is used both to map ACQID to the right slot and
     # to get the star positions to estimate deltas later
     timeline_at_acq = mica.starcheck.starcheck.get_timeline_at_date(manvr.start)
-    mp_dir = None
-    if timeline_at_acq is not None:
-        mp_dir = timeline_at_acq['mp_dir']
+    mp_dir = None if (timeline_at_acq is None) else timeline_at_acq['mp_dir']
     starcheck = mica.starcheck.get_starcheck_catalog(int(obsid),
                                                      mp_dir=mp_dir)
     if 'cat' not in starcheck:
