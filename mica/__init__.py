3b5ac06e97fbc6264af36a58de333a9a9290da21
Tom Aldcroft <taldcroft@gmail.com>
2016-12-23 13:19:41 -0500

    Update to use testr infrastructure for self testing

--- mica/__init__.py
+++ mica/__init__.py
@@ -1,8 +1,8 @@
 from .version import __version__
 
 def test(*args, **kwargs):
-    """
+    '''
     Run py.test unit tests.
-    """
+    '''
     import testr
     return testr.test(*args, **kwargs)
