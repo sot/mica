c9b4b68487efb4039d3f08c6ad03131479ef86ff
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-02-03 08:59:38 -0500

    Add back a dark_model.py file to import the chandra_aca versions

--- mica/archive/aca_dark/__init__.py
+++ mica/archive/aca_dark/__init__.py
@@ -1,2 +1,1 @@
 from .dark_cal import *
-from chandra_aca.dark_model import *
---
+++ mica/archive/aca_dark/dark_model.py
@@ -0,0 +1,3 @@
+import warnings
+warnings.warn("mica.archive.aca_dark.dark_model is deprecated.  See chandra_aca.dark_model")
+from chandra_aca.dark_model import *
