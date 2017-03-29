c246f0e9add0a5b6d7fce9b9e2107671440b5f90
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-03-29 12:34:19 -0400

    Update test script to use a provided start time

--- mica/starcheck/tests/make_database.py
+++ mica/starcheck/tests/make_database.py
@@ -11,12 +11,9 @@ mica.common.MICA_ARCHIVE = TESTDIR
 import mica.starcheck.process
 
 # Just ingest files from the last couple of weeks or so
-# This still uses the silly find files newer than this other file method, so
-# set the time stamp on that reference file
-if not os.path.exists(os.path.join(TESTDIR, 'starcheck')):
-    os.makedirs(os.path.join(TESTDIR, 'starcheck'))
-bash("touch -d {} {}".format(DateTime(-15).iso, mica.starcheck.process.FILES['touch_file']))
-# And just check that the update script didn't raise any exceptions
-mica.starcheck.process.update()
+# And just check (implicitly) that the update script didn't raise any exceptions
+config = mica.starcheck.process.DEFAULT_CONFIG
+config['start'] = DateTime() - 30
+mica.starcheck.process.update(config)
 # Cleanup manually
 bash("rm -r {}".format(TESTDIR))
