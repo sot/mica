befb6bc8eaa2d0ba56838b00c5fd86f193c8fdba
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-03-29 13:15:58 -0400

    Update version to 3.9.0 from 3.8.0

--- mica/version.py
+++ mica/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '3.8.0'
+version = '3.9.0'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
