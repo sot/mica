b1369a8beef483231ca2e92189590f4f2dd23366
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-07-02 13:34:14 -0400

    Add a report-making test

---
+++ mica/report/tests/test_write_report.py
@@ -0,0 +1,21 @@
+import tempfile
+import os
+import shutil
+
+from .. import report
+
+def test_write_reports():
+    """
+    Make a report and database
+    """
+    tempdir = tempfile.mkdtemp()
+    # Get a temporary file, but then delete it, because report.py will only
+    # make a new table if the supplied file doesn't exist
+    fh, fn = tempfile.mkstemp(dir=tempdir, suffix='.db3')
+    os.unlink(fn)
+    report.REPORT_ROOT = tempdir
+    report.REPORT_SERVER = fn
+    for obsid in [20001, 15175, 54778]:
+        report.main(obsid)
+    os.unlink(fn)
+    shutil.rmtree(tempdir)
