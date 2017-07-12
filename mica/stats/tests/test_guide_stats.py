ce29f011a72bf695c9b0840ad4c121f85c9fcad1
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-07-12 13:40:47 -0400

    Add test to confirm more reasonable residual std on one obsid/slot

--- mica/stats/tests/test_guide_stats.py
+++ mica/stats/tests/test_guide_stats.py
@@ -7,6 +7,11 @@ from .. import guide_stats
 def test_calc_stats():
     guide_stats.calc_stats(17210)
 
+def test_calc_stats_with_bright_trans():
+    s = guide_stats.calc_stats(17472)
+    # Assert that the std on the slot 7 residuals are reasonable
+    # even in this obsid that had a transition to BRIT
+    assert s[1][7]['dr_std'] < 1
 
 def test_make_gui_stats():
     """
