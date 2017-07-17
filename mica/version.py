901fc4fd1f3ba867b8a98e0b05a1b480f01923b5
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-07-17 13:39:15 -0400

    Update mica version to 3.12

--- mica/version.py
+++ mica/version.py
@@ -14,7 +14,7 @@ NOTE: this code copied from astropy and modified.  Any license restrictions
 therein are applicable.
 """
 
-version = '3.11'
+version = '3.12'
 
 _versplit = version.replace('dev', '').split('.')
 major = int(_versplit[0])
