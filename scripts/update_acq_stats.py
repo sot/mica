917c7feb625b9187c1da5687b602da874c60cbd9
Tom Aldcroft <taldcroft@gmail.com>
2019-06-27 15:06:03 -0400

    Py3 compatibility updates

--- scripts/update_acq_stats.py
+++ scripts/update_acq_stats.py
@@ -8,11 +8,11 @@ import os
 table_file = mica.stats.acq_stats.table_file
 file_stat = os.stat(table_file)
 if file_stat.st_size > 50e6:
-    print """
+    print("""
 Warning: {tfile} is larger than 50MB and may need
 Warning: to be manually repacked (i.e.):
 Warning:
 Warning: ptrepack --chunkshape=auto --propindexes --keep-source-filters {tfile} compressed.h5
 Warning: cp compressed.h5 {tfile}
 
-""".format(tfile=table_file)
+""".format(tfile=table_file))
