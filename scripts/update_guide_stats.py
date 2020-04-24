d13c674a7286f1af9cd13babe2cb5c429b5b3bfa
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-04-24 15:56:45 -0400

    Update guide stat script to pass datafile

--- scripts/update_guide_stats.py
+++ scripts/update_guide_stats.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+import argparse
 
 from mica.stats import update_guide_stats
-update_guide_stats.main()
+import mica.stats.guide_stats
 
-import os
+# Cheat and pass options directly.  Needs entrypoint scripts
+opt = argparse.Namespace(datafile=mica.stats.guide_stats.TABLE_FILE,
+                         obsid=None, check_missing=False, start=None, stop=None)
+update_guide_stats.update(opt)
+
+
 table_file = mica.stats.guide_stats.TABLE_FILE
 file_stat = os.stat(table_file)
 if file_stat.st_size > 200e6:
