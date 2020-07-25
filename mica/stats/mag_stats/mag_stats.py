fix q normalization

mica/stats/mag_stats/mag_stats.py
@@ -137,7 +137,7 @@ def get_star_position(star, slot, telem):
                   telem['AOATTQT4']]).transpose()
     norm = np.sum(q**2, axis=1, keepdims=True)
     # I am just normalizing q, just in case.
-    q_att = Quat(q=q/norm)
+    q_att = Quat(q=q / np.sqrt(norm))
     Ts = q_att.transform
 
     star_pos_eci = Ska.quatutil.radec2eci(star['RA_PMCORR'], star['DEC_PMCORR'])