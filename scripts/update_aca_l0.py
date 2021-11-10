3cdd149bfaf0a7d77cffd19f35ea8a3afc7dcdc3
Tom Aldcroft <taldcroft@gmail.com>
2021-11-10 12:13:32 -0500

    Make new cda subpackage and mv cda_aca0_list there

--- scripts/update_aca_l0.py
+++ scripts/update_aca_l0.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import mica.archive.cda_aca0_list
+import mica.archive.cda.aca0_list
 import mica.archive.aca_l0
 
-mica.archive.cda_aca0_list.main()
+mica.archive.cda.aca0_list.main()
 mica.archive.aca_l0.main()
