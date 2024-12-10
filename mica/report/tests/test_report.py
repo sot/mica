48c367c6f8ac908cc8d009da43f3c6cc6c1c8e82
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-12-10 16:23:39 -0500

    Ruff format new test file

--- mica/report/tests/test_report.py
+++ mica/report/tests/test_report.py
@@ -15,10 +15,10 @@ def test_target_summary_or():
     assert summary["lts_lt_plan"] is None
     assert summary["soe_st_sched_date"] == "Nov 14 2000 12:49AM"
 
+
 def test_target_summary_er():
     """
     Test that target_summary for an ER obsid returns None"""
     obsid = 54000
     summary = mica.report.report.target_summary(obsid)
     assert summary is None
-
